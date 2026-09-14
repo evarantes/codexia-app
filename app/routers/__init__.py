9f0d3a88ff525e51e7ce8511a1abb266351f5e94
evarantes <evarantes2@gmail.com>
2026-09-14 12:57:04 -0300

    feat: mount cinematic hybrid composition routes

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -7,6 +7,8 @@ changing the large legacy router.
 from . import pydantic_compat as _pydantic_compat  # noqa: F401
 from . import youtube as youtube
 from .cinematic_campaign import router as _cinematic_router
+from .cinematic_compose import router as _cinematic_compose_router
 
 # Safe to execute once per process: package __init__ is cached by Python.
 youtube.router.include_router(_cinematic_router)
+youtube.router.include_router(_cinematic_compose_router)
